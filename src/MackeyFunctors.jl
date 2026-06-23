348138e46757925d1384df93d11c83778d907241
Thomas Brazelton <42276623+tbrazel@users.noreply.github.com>
2026-06-23 16:49:29 -0400

    removed old files

--- src/MackeyFunctors.jl
+++ src/MackeyFunctors.jl
@@ -2,18 +2,11 @@ module MackeyFunctors
 using GAP
 using AbstractAlgebra
 
-include("MiscFuncs.jl")
-
 include("module_methods/Modules.jl")
 
-include("group_theory/AbelianGroupHom.jl")
-export defines_fg_abelian_map,
-    are_equal_abelian_group_homomorphisms # don't need to export these long term
 
 include("group_theory/Words.jl")
 export generator_relations
-
-include("group_theory/HelperFunctions.jl")
 
 include("types/MackeyContext.jl")
 export MackeyContext, 
@@ -24,11 +17,5 @@ export MackeyContext,
 
 include("types/MackeyFunctor.jl")
 export MackeyFunctor
-
-include("group_theory/SubgroupLattice.jl")
-export all_supergroups_from_lattice
-
-include("visualizers/MackeyFunctorVisualizerData.jl")
-export visualizer_data, visualizer_json
 
 end
